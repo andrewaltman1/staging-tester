7309d309c518fff68b3700c1495465a68f65402f
andrewaltman1 <55675406+andrewaltman1@users.noreply.github.com>
2022-09-22 11:43:58 -0400

    changed port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const ejsMate = require("ejs-mate");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = 443;
 
 app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
